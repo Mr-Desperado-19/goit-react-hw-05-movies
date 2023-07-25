0e2c4c9904bc7de617d75518806d4992513c182f
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-07-25 22:50:03 +0200

    Update Home & Movies

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,45 +1,9 @@
-// import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
-// import { getTrendingMovies } from '../../services/api';
-
-// function Home() {
-//   const [trendingMovies, setTrendingMovies] = useState([]);
-
-//   useEffect(() => {
-//     async function fetchTrendingMovies() {
-//       const movies = await getTrendingMovies();
-//       setTrendingMovies(movies.results);
-//     }
-//     fetchTrendingMovies();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Популярні фільми</h1>
-//       <ul>
-//         {trendingMovies.map((movie) => (
-//           <li key={movie.id}>
-//             <a href={`/movies/${movie.id}`}>{movie.title}</a>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// Home.propTypes = {
-//   getTrendingMovies: PropTypes.func.isRequired,
-// };
-
-// export default Home;
-
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Outlet } from 'react-router-dom';
 import { getTrendingMovies } from '../../services/api';
 
 function Home() {
-  const [setTrendingMovies] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
   useEffect(() => {
     async function fetchTrendingMovies() {
@@ -52,7 +16,13 @@ function Home() {
   return (
     <div>
       <h1>Популярні фільми</h1>
-      <Outlet />
+      <ul>
+        {trendingMovies.map((movie) => (
+          <li key={movie.id}>
+            <a href={`/movies/${movie.id}`}>{movie.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
@@ -62,3 +32,33 @@ Home.propTypes = {
 };
 
 export default Home;
+
+// import React, { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { Outlet } from 'react-router-dom';
+// import { getTrendingMovies } from '../../services/api';
+
+// function Home() {
+//   const [setTrendingMovies] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchTrendingMovies() {
+//       const movies = await getTrendingMovies();
+//       setTrendingMovies(movies.results);
+//     }
+//     fetchTrendingMovies();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Популярні фільми</h1>
+//       <Outlet />
+//     </div>
+//   );
+// }
+
+// Home.propTypes = {
+//   getTrendingMovies: PropTypes.func.isRequired,
+// };
+
+// export default Home;
--- src/page/movies/Movies.jsx
+++ src/page/movies/Movies.jsx
@@ -1,52 +1,10 @@
-// import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
-// import { searchMovies } from '../../services/api';
-
-// function Movies() {
-//   const [query, setQuery] = useState('');
-//   const [searchResults, setSearchResults] = useState([]);
-
-//   const handleSearch = async (event) => {
-//     event.preventDefault();
-//     const results = await searchMovies(query);
-//     setSearchResults(results);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Пошук фільмів</h1>
-//       <form onSubmit={handleSearch}>
-//         <input type="text" value={query} onChange={(event) => setQuery(event.target.value)} />
-//         <button type="submit">Пошук</button>
-//       </form>
-//       {searchResults.length > 0 && (
-//         <ul>
-//           {searchResults.map((movie) => (
-//             <li key={movie.id}>
-//               <a href={`/movies/${movie.id}`}>{movie.title}</a>
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// }
-
-// Movies.propTypes = {
-//   searchMovies: PropTypes.func.isRequired,
-// };
-
-// export default Movies;
-
-
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Outlet } from 'react-router-dom';
 import { searchMovies } from '../../services/api';
 
 function Movies() {
   const [query, setQuery] = useState('');
-  const [setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearch = async (event) => {
     event.preventDefault();
@@ -61,7 +19,15 @@ function Movies() {
         <input type="text" value={query} onChange={(event) => setQuery(event.target.value)} />
         <button type="submit">Пошук</button>
       </form>
-      <Outlet />
+      {searchResults.length > 0 && (
+        <ul>
+          {searchResults.map((movie) => (
+            <li key={movie.id}>
+              <a href={`/movies/${movie.id}`}>{movie.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
@@ -71,3 +37,37 @@ Movies.propTypes = {
 };
 
 export default Movies;
+
+
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import { Outlet } from 'react-router-dom';
+// import { searchMovies } from '../../services/api';
+
+// function Movies() {
+//   const [query, setQuery] = useState('');
+//   const [setSearchResults] = useState([]);
+
+//   const handleSearch = async (event) => {
+//     event.preventDefault();
+//     const results = await searchMovies(query);
+//     setSearchResults(results);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Пошук фільмів</h1>
+//       <form onSubmit={handleSearch}>
+//         <input type="text" value={query} onChange={(event) => setQuery(event.target.value)} />
+//         <button type="submit">Пошук</button>
+//       </form>
+//       <Outlet />
+//     </div>
+//   );
+// }
+
+// Movies.propTypes = {
+//   searchMovies: PropTypes.func.isRequired,
+// };
+
+// export default Movies;
